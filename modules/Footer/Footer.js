b9e8559659d0687662bf153799a4eb225478168e
Vneder <4007mj@gmail.com>
2024-03-06 10:51:57 +0100

    update variables and styles

--- modules/Footer/Footer.js
+++ modules/Footer/Footer.js
@@ -1,5 +1,4 @@
 const currentYear = new Date().getFullYear();
-
-// const copyrightInfo = document.getElementById('copyright-info');
 const copyrightInfo = document.querySelector('.footer__text');
+
 copyrightInfo.innerHTML = `&copy; ${currentYear} Bariera Zero`;
