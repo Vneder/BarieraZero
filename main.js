7db19ddf0ead3ed78252d3bb3ac4426a0f10ba9a
Vneder <4007mj@gmail.com>
2024-03-11 14:41:30 +0100

    Update construction webiste Co-authored-by: malinka32, Walus0105

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 import './modules/Footer/Footer.js';
 
 // Jakiś tam skrypt na całą stronkę
+// TO DO
 
-
+//     - ikonki social mediów
+//       Szablony do kolejnych stron(te z menu + wydarzenia)
+//             - odstępy pionowe miedzy sekcjami
+//                 -
