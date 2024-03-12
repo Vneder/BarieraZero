bebdf5c8acb6d7444fe0c70386110f55a51c853e
Łukasz <98317104+Vneder@users.noreply.github.com>
2024-03-12 13:40:36 +0100

    to do update

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
 import './modules/Footer/Footer.js';
-
-// Jakiś tam skrypt na całą stronkę
-// TO DO
-
-//     - ikonki social mediów
-//       Szablony do kolejnych stron(te z menu + wydarzenia)
-//             - odstępy pionowe miedzy sekcjami
-//                 -
