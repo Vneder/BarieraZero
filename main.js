ee2c3bc0fc3cdc8286253ef3910d98c31bb24290
Vneder <4007mj@gmail.com>
2024-04-22 22:34:04 +0200

    "Added 'Go to top' arrow, updated scripts and CSS for scrolling functionality, and made minor adjustments to HTML structure in various files" Co-authored-by: malinka32

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 import './modules/Footer/Footer.js';
+
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+function topFunction() {
+    if ('scrollBehavior' in document.documentElement.style) {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    } else {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+}
+
+mybutton.addEventListener("click", function () {
+    topFunction();
+});
